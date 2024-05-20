aa1642dba62c54fefdfbfe2bc658723b2a51234c
AnaHita <anahitaardakanian@gmail.com>
2024-05-20 23:30:01 +0330

    test4

--- Main.java
+++ Main.java
@@ -1,12 +1,2 @@
-import MyPackage.GameController;
-
-public class Main {
-    public static void main(String[] args) {
-        GameController Controller=new GameController();
-        Controller.Run();
-        //this is for test
-
-
-    }
-
-}
+//import MyPackage.GameController;
+//test completed
